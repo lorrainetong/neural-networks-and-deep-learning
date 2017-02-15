3745c46273e18a89e59a3aedb5c8ec63814ed099
Tong Qiao <lovelylorraine@hotmail.com>
2017-02-15 20:38:36 +0000

    Add cross-validation for tuning parameters

--- src/mnist_svm.py
+++ src/mnist_svm.py
@@ -6,22 +6,43 @@ A classifier program for recognizing handwritten digits from the MNIST
 data set, using an SVM classifier."""
 
 #### Libraries
-# My libraries
-import mnist_loader 
-
+# Libraries
+import os
 # Third-party libraries
-from sklearn import svm
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+# Change working directory
+os.chdir('/Users/tongqiao/neural-networks-and-deep-learning/src')
+import mnist_loader
+os.chdir('/Users/tongqiao/neural-networks-and-deep-learning')
 
 def svm_baseline():
     training_data, validation_data, test_data = mnist_loader.load_data()
+
+    # 5-fold cross-validation on 1000 samples, uncomment this section to
+    # get best parameters
+    # ------------------------------------------------------------------
+    # C_range = np.logspace(-5, 5, 11, base = 2)
+    # gamma_range = np.logspace(-5, 5, 11, base = 2)
+    # param_grid = dict(gamma = gamma_range, C = C_range)
+    # cv = KFold(n_splits = 5)
+    # grid = GridSearchCV(SVC(), param_grid = param_grid, cv = cv)
+    # grid.fit(training_data[0][0:1000], training_data[1][0:1000])
+    # print("The best parameters are %s with a score of %0.2f"
+    #   % (grid.best_params_, grid.best_score_))
+    # ------------------------------------------------------------------
+
     # train
-    clf = svm.SVC()
+    clf = SVC(C = 2, gamma = 0.03125)
     clf.fit(training_data[0], training_data[1])
     # test
     predictions = [int(a) for a in clf.predict(test_data[0])]
     num_correct = sum(int(a == y) for a, y in zip(predictions, test_data[1]))
-    print "Baseline classifier using an SVM."
-    print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    print("Baseline classifier using an SVM.")
+    print("%s of %s values correct." % (num_correct, len(test_data[1])))
 
 if __name__ == "__main__":
     svm_baseline()
